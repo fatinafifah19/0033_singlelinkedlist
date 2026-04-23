9fd66a7e759566842dab0efbd32fcc4e74de7ff7
fatinafifah19-sketch <aetata81@gmail.com>
2026-04-23 15:47:02 +0700

    menangani kondisi insert dan set pointer previous, current

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -29,7 +29,20 @@ class LinkedList
         Node *nodeBaru = new Node;
         nodeBaru->noMhs = nim;
 
+if (START == NULL || nim <= START->noMhs)
+     {
+        if (START != NULL && nim == START->noMhs)
+        {
+            cout << "/nDuplikasi noMhs tidak diijinkan\n";
+            return;
+        }
 
-     
+        nodeBaru->next =START;
+        START = nodeBaru;
+        return;
+     }
+
+     Node *previous =START;
+     Node *current = START;
 
      
